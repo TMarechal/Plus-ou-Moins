b81b09b7e533617e188aec4cbf9d4d74c88e7453
thomascodeur <43343330+thomascodeur@users.noreply.github.com>
2018-10-10 14:04:35 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
-var Random = nb_random(0,100);
+var Random = nb_random(0, 101);
 var SaisieN;
 var Compteur;
+var ButtonV = document.getElementById('Valider');
 
-function nb_random(min, max)
-{
-     var Random =Math.random()*(max-min)+min;
-     return Math.floor(Random);
+function nb_random(min, max) {
+    var Random = Math.random() * (max - min) + min;
+    return Math.floor(Random);
 }
 
+console.log(Random);
+ButtonV.addEventListener('click', Validation)
+function Validation() {
+    var Random = nb_random(0, 101);
+    var SaisieN = document.getElementById('Saisie').value;
+    if (SaisieN == Random) {
+        alert('Gagné');
+        return false;
+    }
+    else if (SaisieN < Random) {
+        Compteur= Compteur+1;
+        alert('Plus grand');
+    }
+    if (SaisieN > Random) {
+        Compteur= Compteur+1;
+        alert('Plus petit');
+    }
+
+}
